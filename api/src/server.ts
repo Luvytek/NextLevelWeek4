5ca66ef138f57ab8c0175704341fb25da7ac7ebe
Luvytek <matheusluvison18@outlook.com>
2021-02-22 16:30:39 -0300

    fim do primeiro dia

---
+++ api/src/server.ts
@@ -0,0 +1,15 @@
+import express, { request, response } from 'express'
+
+const app = express()
+
+app.get("/", (request, response) => {
+  return response.json({message:"hello world"})
+})
+
+app.post("/", (request, response) => {
+
+  return response.json({message: " os dados foram salvos com sucesso"})
+})
+
+
+app.listen(3333, () => console.log("Server is running"))
